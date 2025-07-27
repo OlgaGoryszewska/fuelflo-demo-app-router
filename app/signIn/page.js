bcfd903ef3f5b81647b671d0e35291ec9cd62025
olga goryszewska <olga.goryszewska@gmail.com>
2025-07-27 14:10:51 +0530

    addin variable

--- app/signIn/page.js
+++ app/signIn/page.js
@@ -4,7 +4,7 @@ import AuthForm from '../../components/AuthForm';
 
 export default function SignInPage() {
   return (
-    <div class='background-container'>
+    <div className='background-container'>
       <AuthForm />
     </div>
   );
