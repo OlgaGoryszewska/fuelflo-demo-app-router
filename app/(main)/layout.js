dfe6eebcfa7a1682343ade115fd9ec3859a898c9
olga goryszewska <olga.goryszewska@gmail.com>
2025-07-28 14:22:49 +0530

    ading system in the files

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,8 @@
-import './globals.css';
+import '../globals.css';
 import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 import { Poppins } from 'next/font/google';
 import { Roboto_Flex } from 'next/font/google';
-import Head from 'next/head';
-import Menu from '@/components/Menu';
+
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,14 +20,14 @@ export default function DashboardLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable} ${roboto.variable}`}>
       <body>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded"
-            rel="stylesheet"
-          />
-        </Head>
-      <Menu />
-        
+       
+        <div className="navAvatar">
+          <div>
+            <h3>Name</h3>
+            <h3>ID: 7767867</h3>
+          </div>
+          <img src="/avatar.png" alt="Avatar" className="avatar" />
+        </div>
         <main>{children}</main>
       </body>
     </html>
--- app/signIn/page.js
+++ app/signIn/page.js
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic';
 
-import AuthForm from '../../components/AuthForm';
+import AuthForm from '/components/AuthForm';
 
 export default function SignInPage() {
   return (
