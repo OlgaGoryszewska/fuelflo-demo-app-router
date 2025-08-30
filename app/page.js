9c1151d6f4d510a0d5a5f9b4586a2cf243fff007
olga goryszewska <olga.goryszewska@gmail.com>
2025-08-30 22:34:35 +0530

    adding logo and roles

--- app/page.js
+++ app/page.js
@@ -1,3 +1,18 @@
+const roleBlocks=[
+  {
+    title:"For Managers & Hire Desk",
+  },
+  {
+    title:"For Technicians",  
+  },
+  {
+    title:"For Fuel Suppliers",
+  },
+  {
+    title:"For Event Organisers",
+  },
+];
+
 export default function Home() {
   return (
     <div>
@@ -11,9 +26,18 @@ export default function Home() {
         <button>
           <a href="/signIn">Sign In</a>
         </button>
-        <p className="body-text pt-4 border-t border-dashed border-s-cyan-700">We are here to support your success—every
-        step of the way.</p>
+        <p className="body-text pt-4 ">“We built Flo right into the FuelFlo Solution App so every role can work faster and with confidence. From forecasting and delivery proof to clean dashboards and alerts, Flo guides your team step-by-step and makes sure nothing slips.”</p>
         <img src="/hello-blob.png" alt="main page image" className="pb-4 " />
+        <h2 className="text-4xl text-center pt-4">Benefits by Role</h2>
+        {roleBlocks.map((roleBlock)=>(
+          <div key={roleBlock.title} className="border border-gray-300 rounded-lg p-4 m-4 w-full max-w-md shadow-md">
+            <h3 className="text-2xl mb-2">{roleBlock.title}</h3>
+           
+          </div>
+
+        ))}
+
+       
       </div>
     </div>
   );
