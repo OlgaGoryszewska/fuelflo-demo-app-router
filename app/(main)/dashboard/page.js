affeb4629c403183c530d9ac0ff565ebb028fca7
olga goryszewska <olga.goryszewska@gmail.com>
2025-10-28 11:15:50 +0530

    ui and ux

--- app/(main)/dashboard/page.js
+++ app/(main)/dashboard/page.js
@@ -1,13 +1,49 @@
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
+import { ChevronDown } from "lucide-react";
+
+
+
 
 export default function Dashboard() {
+  const [openCard, setOpenCard] = useState(false);
   return (
     <div>
       <div className="m-2.5">
-        <Link href="/projects/" className="form-button">
+        <div 
+        onClick={() => setOpenCard(!openCard)} 
+        className={`form-button ${openCard ? 'border-active' : ''}`}>
           <span className="material-symbols-outlined">tactic</span>
-          Projects
-        </Link>
+          Projects<ChevronDown className={`ml-auto transition-transform ${openCard ? 'rotate-180' : ''}`} />
+        </div>
+        {openCard && (<div className="open-card ">
+          
+          <Link href="/add-new-project" className="card-button">
+            <span className="material-symbols-outlined">add</span>
+            Add new Project
+          </Link>
+          <Link href="/ongoing-projects-page" className="card-button">
+            <span className="material-symbols-outlined">workspaces</span>
+            Ongoing Projects
+          </Link>
+          <Link href="/" className="card-button">
+            <span className="material-symbols-outlined">archive</span>
+            Archive
+          </Link>
+        </div>)}
+      
+
+
+
+
+
+
+
+
+
+
+      
         <Link href="/projects/" className="form-button">
           <span className="material-symbols-outlined">engineering</span>
           Technicians
