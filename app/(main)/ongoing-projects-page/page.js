268b43bab4cdfe35060ffad8add2208ea92a4006
olga goryszewska <olga.goryszewska@gmail.com>
2025-10-28 15:52:58 +0530

    same styling

--- app/(main)/ongoing-projects-page/page.js
+++ app/(main)/ongoing-projects-page/page.js
@@ -27,7 +27,7 @@ export default function OngoingProjectsPage() {
     <div className="main-container">
       <div>
         <div className="form-header mb-4">
-          <span className="material-symbols-outlined big">workspaces</span>
+          
           <h1 className="ml-2">Ongoing Projects</h1>
         </div>
         <ul className="flex flex-col gap-2">
