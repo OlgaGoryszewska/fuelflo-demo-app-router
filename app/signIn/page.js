1a96d282e375a5a27d79405fbab1c730c96de209
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2025-12-08 16:18:06 +0530

    changing path

--- app/signIn/page.js
+++ app/signIn/page.js
@@ -1,6 +1,5 @@
 export const dynamic = 'force-dynamic';
-
-import AuthForm from '/components/AuthForm';
+import AuthForm from '../../components/AuthForm';
 
 export default function SignInPage() {
   return (
