bd57c6db449155272f552434cc4dd7724748cace
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2025-12-08 07:13:59 +0530

    toggling

---
+++ app/(main)/create-qr-code/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+export default function CreateQrCodePage() {
+  return (
+    <div className="main-container">
+      <div className="form-header mb-4">
+        <h1 className="ml-2">Create QR Code</h1>
+      </div>
+      <div className="background-container-white">
+        {/* Content for creating QR code goes here */}
+      </div>
+    </div>
+  );
+}
