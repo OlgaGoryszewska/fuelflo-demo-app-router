91f7c5be6b4ca7d026f4ff64b606021b4cfa1a5c
olga goryszewska <olga.goryszewska@gmail.com>
2025-12-19 13:20:35 +0530

    adding pages

---
+++ app/(main)/add-fuel-transactions-page/fuel-delivery-form/page.js
@@ -0,0 +1,13 @@
+export function FuelDeliveryFormPage() {
+  return (
+    <div className="main-container">
+      <div className="form-header mb-4">
+        <h1 className="ml-2">Fuel Delivery Form</h1>
+      </div>
+      <div className="background-container-white">
+        {/* Fuel Delivery Form content goes here */}
+        <p>This is where the fuel delivery form will be implemented.</p>
+      </div>
+    </div>
+  );
+}
