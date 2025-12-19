cc35f550636a09e06fde1e9566e2bb197c5b3bc5
olga goryszewska <olga.goryszewska@gmail.com>
2025-12-19 13:29:54 +0530

    error fix

--- app/(main)/add-fuel-transactions-page/fuel-delivery-form/page.js
+++ app/(main)/add-fuel-transactions-page/fuel-delivery-form/page.js
@@ -1,13 +1,5 @@
-export function FuelDeliveryFormPage() {
-  return (
-    <div className="main-container">
-      <div className="form-header mb-4">
-        <h1 className="ml-2">Fuel Delivery Form</h1>
-      </div>
-      <div className="background-container-white">
-        {/* Fuel Delivery Form content goes here */}
-        <p>This is where the fuel delivery form will be implemented.</p>
-      </div>
-    </div>
-  );
+"use client";
+
+export default function Page() {
+  return <div>Fuel delivery form</div>;
 }
--- app/(main)/add-fuel-transactions-page/fuel-return-form/page.js
+++ app/(main)/add-fuel-transactions-page/fuel-return-form/page.js
@@ -1,13 +1,5 @@
-export function FuelReturnFormPage() {
-  return (
-    <div className="main-container">
-      <div className="form-header mb-4">
-        <h1 className="ml-2">Fuel Return Form</h1>
-      </div>
-      <div className="background-container-white">
-        {/* Fuel Return Form content goes here */}
-        <p>This is where the fuel return form will be implemented.</p>
-      </div>
-    </div>
-  );
+"use client";
+
+export default function Page() {
+  return <div>Fuel delivery form</div>;
 }
