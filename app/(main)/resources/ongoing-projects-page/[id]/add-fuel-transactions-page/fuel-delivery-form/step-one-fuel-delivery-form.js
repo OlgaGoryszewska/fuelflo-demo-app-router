3edee8fd4656c136f5ee21d9084e2be507171241
olga goryszewska <olga.goryszewska@gmail.com>
2026-02-28 23:35:41 +0530

    changing img

--- app/(main)/resources/ongoing-projects-page/[id]/add-fuel-transactions-page/fuel-delivery-form/step-one-fuel-delivery-form.js
+++ app/(main)/resources/ongoing-projects-page/[id]/add-fuel-transactions-page/fuel-delivery-form/step-one-fuel-delivery-form.js
@@ -7,7 +7,7 @@ import TankDropdown from '@/components/dropdowns/tank-dropdown';
 export default function StepOneFuelDeliveryForm() {
   return (
     <form className="form-no-style">
-      <Image src={delivery} alt="Description of my image" className="mb-4" />
+      <Image src={delivery} alt="Description of my image" className="m-auto w-42" />
 
       <button className="button-big-border-orange mb-4 ">
         <span className="material-symbols-outlined black pr-2">qr_code</span>
--- app/(main)/resources/ongoing-projects-page/[id]/add-fuel-transactions-page/fuel-return-form/step-one-fuel-return-form.js
+++ app/(main)/resources/ongoing-projects-page/[id]/add-fuel-transactions-page/fuel-return-form/step-one-fuel-return-form.js
@@ -7,7 +7,8 @@ import TankDropdown from '@/components/dropdowns/tank-dropdown';
 export default function FuelReturnForm() {
   return (
     <form className="form-no-style">
-      <Image src={returnImage} alt="Description of my image" className="mb-4" />
+      <Image src={returnImage} alt="Description of my image" className=" m-auto w-42
+      " />
       <button className="button-big-border-orange mb-4 ">
         <span className="material-symbols-outlined black pr-2">qr_code</span>
         Skan Generator QR Code
