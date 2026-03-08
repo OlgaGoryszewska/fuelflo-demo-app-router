bced8ea0b136984535d829e203d836e5318d8e70
olga goryszewska <olga.goryszewska@gmail.com>
2026-03-08 23:14:16 +0530

    data

--- components/fuel-transaction/Intro.js
+++ components/fuel-transaction/Intro.js
@@ -11,26 +11,25 @@ export default function IntroForm() {
         <p className="steps-text pr-2">Instruction</p>
       </div>
       <section>
-        <h2>Collect tank refill evidence in 5 steps</h2>
+        <h2>Collect tank refill data in 4 steps</h2>
         <Image src={scan}  alt=" scaning meter" className="h-42 w-42 m-auto
         "/>
 
         <ol className="steps">
         <li>
            
-            <strong className="highlight"> 1. Setup</strong>
+            <strong className="highlight"> 1. Setup operation </strong>
           </li>
             
           <li>
           <strong className="highlight">2. </strong>  Take a photo of the 
-            <strong className="highlight"> starting</strong> meter
+            <strong className="highlight"> starting</strong> meter and enter meter reading
           </li>
-          <li><strong className="highlight">3. </strong> Scan or enter the meter reading</li>
-          <li><strong className="highlight">4. </strong> Take a photo
-            <strong className="highlight"> after </strong> the tank refill
+          <li><strong className="highlight">3. </strong> Take a photo
+            <strong className="highlight"> after </strong> the tank refill and enter meter current status
           </li>
           <li>
-          <strong className="highlight">5. </strong> Scan or enter the final meter reading
+          <strong className="highlight">4. </strong>Review fuel data
           </li>
         </ol>
       </section>
