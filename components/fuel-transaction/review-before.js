f96eb396a1d51d7edcebd440cd6f57dfd6fe09e4
olga goryszewska <olga.goryszewska@gmail.com>
2026-03-12 11:05:20 +0530

    review

--- components/fuel-transaction/review-before.js
+++ components/fuel-transaction/review-before.js
@@ -1,13 +1,13 @@
 'use client';
 
-export default function ReviewBefore() {
+export default function ReviewBefore({formData}) {
   return (
     <div>
       <div className="form-header-steps">
         <p className="steps-text pr-2">Step 3 of 5</p>
       </div>
       <h2 className="mt-4">Review </h2>
-      <p className="mt-4 h-mid-gray-s">Generator:</p>
+      <p className="mt-4 h-mid-gray-s">Generator:{formData.generator_id}</p>
       <div className="divider-full"></div>
       <p className="mt-4 h-mid-gray-s">Tank:</p>
       <div className="divider-full"></div>
