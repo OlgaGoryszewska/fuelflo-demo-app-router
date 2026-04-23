b203355d9f0ba95043da6760c852b8b2440576c9
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2026-04-23 13:11:17 +1200

    test

--- components/add_new_project/StepFive.js
+++ components/add_new_project/StepFive.js
@@ -9,82 +9,86 @@ export default function StepFive({ formData }) {
       <p className="steps-text">Review Before Submitting</p>
       <div className="divider-full mb-4"></div>
 
-      <h2>Project Specification</h2>
+      <h4 className="my-2 ">Project Specification</h4>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+      <p className="steps-text">Name of the Project</p>
 
-      <h4>Name of the Project</h4>
+      <p className="h-mid-gray-s ">{formData.name || '-'}</p>
+    </div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+      <p className="steps-text">Localisation of the Project</p>
 
-      <p className="steps-text mb-2">{formData.name || '-'}</p>
-
-      <h4>Localisation of the Project</h4>
-
-      <p className="steps-text mb-2">{formData.location || '-'}</p>
-
-      <h4>Release Date</h4>
-
-      <p className="steps-text mb-2">{formData.start_date || '-'}</p>
-
-      <div className="flex flex-row">
-        <h4>End Date</h4>
+      <p className="h-mid-gray-s">{formData.location || '-'}</p>
       </div>
-      <p className="steps-text mb-2">{formData.end_date || '-'}</p>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+      <p className="steps-text">Release Date</p>
 
-      <h2>Contact to Contractor</h2>
-
-      <div className="flex flex-row">
-        <h4>Name of the Contractor</h4>
+      <p className="h-mid-gray-s ">{formData.start_date || '-'}</p>
       </div>
-      <p className="steps-text mb-2">{formData.contractor_name || '-'}</p>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+  
+      <p className="steps-text">End Date</p>
+       
+    
+      <p className="h-mid-gray-s ">{formData.end_date || '-'}</p>
+  </div>
+      <h4 className="my-2 ">Contact to Contractor</h4>
 
-      <div className="flex flex-row">
-        <h4>Address of the Contractor</h4>
-      </div>
-      <p className="steps-text mb-2">{formData.contractor_address || '-'}</p>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text ">Name of the Contractor</p>
+     
+      <p className="h-mid-gray-s ">{formData.contractor_name || '-'}</p>
+ </div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text ">Address of the Contractor</p>
+    
+      <p className="h-mid-gray-s">{formData.contractor_address || '-'}</p>
+  </div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text ">Email</p>
+      
+      <p className="h-mid-gray-s">{formData.email || '-'}</p>
+</div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text ">Mobile</p>
+     
+      <p className="h-mid-gray-s ">{formData.mobile || '-'}</p>
+    </div>
+      <h4 className="my-2 ">Setup</h4>
 
-      <div className="flex flex-row">
-        <h4>Email</h4>
-      </div>
-      <p className="steps-text mb-2">{formData.email || '-'}</p>
-
-      <div className="flex flex-row">
-        <h4>Mobile</h4>
-      </div>
-      <p className="steps-text mb-2">{formData.mobile || '-'}</p>
-
-      <h2>Setup</h2>
-
-      <div className="flex flex-row">
-        <h4>Manager</h4>
-      </div>
-      <p className="steps-text mb-2">{formData.manager?.name || '-'}</p>
-
-      <div className="flex flex-row">
-        <h4>Technicians</h4>
-      </div>
-      <p className="steps-text mb-2">
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text">Manager</p>
+      
+      <p className="h-mid-gray-s mb-2">{formData.manager?.name || '-'}</p>
+</div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text">Technicians</p>
+     
+      <p className="h-mid-gray-s  mb-2">
         {technicians.length > 0
           ? technicians.map((tech) => tech.name).join(', ')
           : 'None'}
       </p>
-
-      <div className="flex flex-row">
-        <h4>Generators and Tanks</h4>
-      </div>
+ </div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text">Generators and Tanks</p>
+     
 
       {generators.length === 0 ? (
-        <p className="steps-text mb-2">No generators assigned</p>
+        <p className="h-mid-gray-s ">No generators assigned</p>
       ) : (
         <div className="flex flex-col">
           {generators.map((gen) => (
             <div key={gen.id}>
-              <p className="steps-text mb-2 font-semibold"> Generator: {gen.name}</p>
+              <p className="h-mid-gray-s "> Generator {gen.name}</p>
 
               {(gen.tanks || []).length === 0 ? (
-                <p className="steps-text mb-2">No tanks assigned</p>
+                <p className="h-mid-gray-s ">No tanks assigned</p>
               ) : (
                 <ul>
                   {(gen.tanks || []).map((tank) => (
-                    <li key={tank.id} className="steps-text ">
-                      {tank.name}
+                    <li key={tank.id} className="h-mid-gray-s">
+                      {tank.name} (tank)
                     </li>
                   ))}
                 </ul>
@@ -92,36 +96,38 @@ export default function StepFive({ formData }) {
             </div>
           ))}
         </div>
-      )}
-
-      <h2>Fuel Pricing Details</h2>
-
-      <div className="flex flex-row">
-        <h4>Purchase Price</h4>
+      )} 
       </div>
-      <p className="steps-text mb-2">{formData.amount || '-'}</p>
 
-      <div className="flex flex-row">
-        <h4>Selling Price</h4>
+      <h4 className="my-2 ">Fuel Pricing Details</h4>
+
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text ">Purchase Price</p>
+      
+      <p className="h-mid-gray-s ">{formData.amount || '-'}</p>
+       </div>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className="steps-text">Selling Price</p>
+     
+      <p className=" h-mid-gray-s ">{formData.selling_price || '-'}</p>
       </div>
-      <p className="steps-text mb-2">{formData.selling_price || '-'}</p>
+      <h4 className="my-2 ">Details</h4>
 
-      <h2>Notes</h2>
-
-      <div className="flex flex-row">
-        <h4>Specification of the project</h4>
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+        <p className=" steps-text ">Specification of the project</p>
+      <p className=" h-mid-gray-s ">{formData.specification || '-'}</p>
       </div>
-      <p className="steps-text mb-2">{formData.specification || '-'}</p>
-
-      <div className="flex flex-row">
-        <h4>Additional Note</h4>
+      <div className=" mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2" >
+        <p className="steps-text ">Additional Note</p>
+        <p className=" h-mid-gray-s  ">{formData.additional || '-'}</p>
       </div>
-      <p className="steps-text mb-2">{formData.additional || '-'}</p>
-
-      <h2>Status</h2>
-      <p className="steps-text mb-2">
+    
+      <div className="mb-2 flex flex-col  rounded-xl text-[#62748e] bg-[#f5fbff] p-2">
+      <p className="steps-text">Status</p>
+      <p className=" h-mid-gray-s">
         {formData.active ? 'Active' : 'Inactive'}
       </p>
+      </div>
     </div>
   );
 }
