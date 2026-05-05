0739da7081ee9a57cf01a11d4fb71babb7999965
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2026-05-06 10:20:22 +1200

    text bar

--- components/PwaBottomMenu.jsx
+++ components/PwaBottomMenu.jsx
@@ -18,24 +18,24 @@ export default function PwaBottomMenu() {
   if (!isPwa) return null;
 
   return (
-    <nav className="fixed bottom-0 left-0 z-50 w-full border-t bg-white px-4 py-2 shadow-lg">
+    <nav className="fixed bottom-0 left-0 z-50 w-full  px-4 py-2 shadow-lg">
       <div className="mx-auto flex max-w-md items-center justify-between">
-        <Link href="/" className="flex flex-col items-center text-xs">
-          <Home size={22} />
-          <span>Home</span>
+       
+      <Link href="/resources/projects" className="flex flex-col items-center text-xs">
+          <Folder size={22} />
+          <span>Projects</span>
         </Link>
-
-        <Link href="/projects" className="flex flex-col items-center text-xs">
+        <Link href="/resources/projects" className="flex flex-col items-center text-xs">
           <Folder size={22} />
           <span>Projects</span>
         </Link>
 
-        <Link href="/add" className="flex flex-col items-center text-xs">
+        <Link href="/resources/projects" className="flex flex-col items-center text-xs">
           <PlusCircle size={26} />
-          <span>Add</span>
+          <span>Add transaction</span>
         </Link>
 
-        <Link href="/profile" className="flex flex-col items-center text-xs">
+        <Link href="/resources/profile" className="flex flex-col items-center text-xs">
           <User size={22} />
           <span>Profile</span>
         </Link>
